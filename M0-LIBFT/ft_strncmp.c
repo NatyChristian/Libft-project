40d5afdc741c9c925df2fd6736d3ca94e91dec36
Christian <MbolanantenainaChristian@gmail.com>
2026-01-29 19:28:11 +0300

    everything is valide

--- M0-LIBFT/ft_strncmp.c
+++ M0-LIBFT/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jmbolana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 15:31:36 by jmbolana          #+#    #+#             */
-/*   Updated: 2026/01/27 15:41:46 by jmbolana         ###   ########.fr       */
+/*   Updated: 2026/01/29 19:27:38 by jmbolana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n &&
-		s1[i] != '\0' &&
-		s2[i] != '\0')
+	while (i < n
+		&& s1[i] != '\0'
+		&& s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
--- M0-LIBFT/libft.h
+++ M0-LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: jmbolana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 11:20:37 by jmbolana          #+#    #+#             */
-/*   Updated: 2026/01/27 15:27:38 by jmbolana         ###   ########.fr       */
+/*   Updated: 2026/01/29 19:27:03 by jmbolana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ size_t		ft_strlen(const char *str);
 size_t		ft_strlcat(char *dest, const char *src, size_t size);
 void		*ft_memchr(const void	*s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_isdigit(int c);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
