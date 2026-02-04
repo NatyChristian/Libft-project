b25378537829839a458133fafc9fff8fdf7925bc
Christian <MbolanantenainaChristian@gmail.com>
2026-02-04 15:46:25 +0300

    update

--- M0-LIBFT/ft_striteri.c
+++ M0-LIBFT/ft_striteri.c
@@ -1,48 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Jmbolana <jmbolana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/04 14:38:16 by Jmbolana          #+#    #+#             */
-/*   Updated: 2026/02/04 14:38:16 by Jmbolana         ###   ########.fr       */
+/*   Created: 2026/02/04 15:32:53 by Jmbolana          #+#    #+#             */
+/*   Updated: 2026/02/04 15:32:53 by Jmbolana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 // #include <stdio.h>
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
-	char			*str;
 
 	if (!s || !f)
-		return (NULL);
+		return ;
 	i = 0;
-	str = ft_calloc(sizeof(char), ft_strlen(s) + 1);
-	if (!str)
-		return (NULL);
 	while (s[i])
 	{
-		str[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	return (str);
 }
 
-// static char	ft_toupperv2(unsigned int i, char ch)
+// static void	ft_toupperv2(unsigned int i, char *ch)
 // {
 // 	(void)i;
-// 	return (ft_toupper(ch));
+// 	*ch = ft_toupper(*ch);
 // }
 
 // int	main(void)
 // {
-// 	char	*strstart = "mbolanantenaina jean philippe christian";
-// 	char	*strdest;
-// 	strdest = ft_strmapi(strstart, ft_toupperv2);
-// 	printf("%s\n", strdest);
+// 	char	strstart[] = "MbolanxxxxaAAantenaina jEaN";
+
+// 	ft_striteri(strstart, ft_toupperv2);
+// 	printf("%s\n", strstart);
 // 	return (0);
 // }
