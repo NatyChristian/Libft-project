c1030f6d2bad78dd8b9f56f1bb964492f67370f8
Jean Philippe Christian Mbolanatenaina <jmbolana@c1r12s1.42antananarivo.mg>
2026-02-07 11:17:24 +0300

    Ndao aloha hikaly

--- M0-LIBFT/ft_putnbr_fd.c
+++ M0-LIBFT/ft_putnbr_fd.c
@@ -11,8 +11,50 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	printf("ft_putnbr_fd test---------------\n");
+// 	printf("test01---------------\n");
+// 	ft_putnbr_fd(0,1);
+// 	printf("\n");
+// 	printf("test02---------------\n");
+// 	ft_putnbr_fd(42,1);
+// 	printf("\n");
+// 	printf("test03-------------\n");
+// 	ft_putnbr_fd(-42,1);
+// 	printf("\n");
+// 	printf("test04---------------\n");
+// 	ft_putnbr_fd(INT_MAX,1);
+// 	printf("\n");
+// 	printf("test05---------------\n");
+// 	ft_putnbr_fd(INT_MIN,1);
+// 	printf("\n");
+// 	return (0);
+// }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	char	c;
+
+	if (n == 0)
+	{
+		write(fd, "0", 1);
+		return ;
+	}
+	if (n == INT_MIN)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	if (n / 10 > 0)
+		ft_putnbr_fd(n / 10, fd);
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
 }
